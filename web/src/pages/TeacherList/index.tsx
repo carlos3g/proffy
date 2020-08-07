fbcb7a29abc46354f81f34c041524f8ccb7cbed0
carlos3g <carlosmesquita156@gmail.com>
2020-08-07 13:17:40 -0300

    organization

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -8,38 +8,38 @@ import Input from "../../components/Input";
 import Select from "../../components/Select";
 
 function TeacherList() {
+  const weekDaysArray = [
+    { value: "0", label: "Domingo" },
+    { value: "1", label: "Segunda" },
+    { value: "2", label: "Terça" },
+    { value: "3", label: "Quarta" },
+    { value: "4", label: "Quinta" },
+    { value: "5", label: "Sexta" },
+    { value: "6", label: "Sabádo" },
+  ];
+
+  const subjectsArray = [
+    { value: "Artes", label: "Artes" },
+    { value: "Inglẽs", label: "Inglẽs" },
+    { value: "História", label: "História" },
+    { value: "Fisíca", label: "Fisíca" },
+    { value: "Quimíca", label: "Quimíca" },
+    { value: "Português", label: "Português" },
+    { value: "Geografia", label: "Geografia" },
+    { value: "Matemática", label: "Matemática" },
+    { value: "Biologia", label: "Biologia" },
+  ];
+
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
         <form id="search-teachers">
-          <Select
-            name="subject"
-            label="Matéria"
-            options={[
-              { value: "Artes", label: "Artes" },
-              { value: "Inglẽs", label: "Inglẽs" },
-              { value: "História", label: "História" },
-              { value: "Fisíca", label: "Fisíca" },
-              { value: "Quimíca", label: "Quimíca" },
-              { value: "Português", label: "Português" },
-              { value: "Geografia", label: "Geografia" },
-              { value: "Matemática", label: "Matemática" },
-              { value: "Biologia", label: "Biologia" },
-            ]}
-          />
+          <Select name="subject" label="Matéria" options={subjectsArray} />
 
           <Select
             name="week_day"
             label="Dia da semana"
-            options={[
-              { value: "0", label: "Domingo" },
-              { value: "1", label: "Segunda" },
-              { value: "2", label: "Terça" },
-              { value: "3", label: "Quarta" },
-              { value: "4", label: "Quinta" },
-              { value: "5", label: "Sexta" },
-              { value: "6", label: "Sabádo" },
-            ]}
+            options={weekDaysArray}
           />
 
           <Input name="time" label="Horário" type="time" />
