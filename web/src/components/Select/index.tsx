7b2be029464cc92c24934925ff96bcf796ce86a2
carlos3g <carlosmesquita156@gmail.com>
2020-08-07 12:37:18 -0300

    Select component

---
+++ web/src/components/Select/index.tsx
@@ -0,0 +1,32 @@
+import React, { SelectHTMLAttributes } from "react";
+import "./styles.css";
+
+interface IProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  label: string;
+  name: string;
+  options: Array<{
+    value: string;
+    label: string;
+  }>;
+}
+
+const Select: React.FC<IProps> = ({ label, name, options, ...rest }) => {
+  return (
+    <div className="select-block">
+      <label htmlFor={name}>{label}</label>
+      <select id={name} {...rest}>
+        <option value="0" disabled selected hidden>
+          Selecione uma opção
+        </option>
+
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default Select;
