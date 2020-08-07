0404084355fe37ef26cfa4695edfed2704ebeef1
carlos3g <carlosmesquita156@gmail.com>
2020-08-07 12:19:24 -0300

    Textarea component

---
+++ web/src/components/Textarea/index.tsx
@@ -0,0 +1,18 @@
+import React, { TextareaHTMLAttributes } from "react";
+import "./styles.css";
+
+interface IProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  label: string;
+  name: string;
+}
+
+const Textarea: React.FC<IProps> = ({ label, name, ...rest }) => {
+  return (
+    <div className="textarea-block">
+      <label htmlFor={name}>{label}</label>
+      <textarea id={name} {...rest} />
+    </div>
+  );
+};
+
+export default Textarea;
