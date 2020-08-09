9a1f9f36cd16dc1d27e859cdf336e95711c2152f
carlos3g <carlosmesquita156@gmail.com>
2020-08-09 16:41:03 -0300

    configuring tabBarNavigator

--- mobile/src/routes/TabsNavigator/index.tsx
+++ mobile/src/routes/TabsNavigator/index.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 
-// packages
+// navigation
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+// packages
+import { Ionicons } from "@expo/vector-icons";
 
 // screens
 import TeacherListScreen from "../../screens/TeacherList";
@@ -11,9 +14,54 @@ const { Navigator, Screen } = createBottomTabNavigator();
 
 function TabsNavigator() {
   return (
-    <Navigator>
-      <Screen name="TeacherList" component={TeacherListScreen} />
-      <Screen name="Favorites" component={FavoritesScreen} />
+    <Navigator
+      tabBarOptions={{
+        style: {
+          elevation: 0,
+          shadowOpacity: 0,
+          height: 64,
+        },
+        tabStyle: {
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+        },
+        iconStyle: {
+          flex: 0,
+          width: 20,
+          height: 20,
+        },
+        labelStyle: {
+          fontFamily: "Archivo_700Bold",
+          fontSize: 13,
+          marginLeft: 16,
+        },
+        inactiveBackgroundColor: "#fafafc",
+        activeBackgroundColor: "#ebebf5",
+        inactiveTintColor: "#c1bccc",
+        activeTintColor: "#32264d",
+      }}
+    >
+      <Screen
+        name="TeacherList"
+        component={TeacherListScreen}
+        options={{
+          tabBarLabel: "Proffys",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="ios-easel" size={size} color={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="Favorites"
+        component={FavoritesScreen}
+        options={{
+          tabBarLabel: "Proffys",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="ios-heart" size={size} color={color} />
+          ),
+        }}
+      />
     </Navigator>
   );
 }
