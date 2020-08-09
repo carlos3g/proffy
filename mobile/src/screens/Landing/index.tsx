4262f20da6cff9f120c6aa872efe538edcabb20e
carlos3g <carlosmesquita156@gmail.com>
2020-08-09 13:03:29 -0300

    Landing Screen

--- mobile/src/screens/Landing/index.tsx
+++ mobile/src/screens/Landing/index.tsx
@@ -1,7 +1,53 @@
 import React from "react";
 
-function Landing({}) {
-  return null;
+// styled
+import {
+  Container,
+  Banner,
+  Title,
+  TitleBold,
+  ButtonsContainer,
+  Button,
+  ButtonImg,
+  ButtonText,
+  TotalConnections,
+  Image,
+} from "./styles";
+
+// assets
+import LandingImg from "../../assets/images/landing.png";
+
+// icons
+import StudyIcon from "../../assets/images/icons/study.png";
+import GiveClassesIcon from "../../assets/images/icons/give-classes.png";
+import HeartIcon from "../../assets/images/icons/heart.png";
+
+function Landing() {
+  return (
+    <Container>
+      <Banner source={LandingImg} />
+      <Title>
+        Seja Bem-vindo, {"\n"}
+        <TitleBold>O que deseja fazer?</TitleBold>
+      </Title>
+
+      <ButtonsContainer>
+        <Button primary>
+          <ButtonImg source={StudyIcon} />
+          <ButtonText>Estudar</ButtonText>
+        </Button>
+
+        <Button>
+          <ButtonImg source={GiveClassesIcon} />
+          <ButtonText>Dar aulas</ButtonText>
+        </Button>
+      </ButtonsContainer>
+
+      <TotalConnections>
+        Total de 200 conexões já realizadas <Image source={HeartIcon} />
+      </TotalConnections>
+    </Container>
+  );
 }
 
 export default Landing;
---
+++ mobile/src/screens/Landing/styles.ts
@@ -0,0 +1,63 @@
+import styled from "styled-components/native";
+
+// types
+import { IButtonProps } from "../../@types/types";
+
+export const Container = styled.View`
+  background-color: #8257e5;
+  flex: 1;
+  justify-content: center;
+  padding: 40px;
+`;
+
+export const Banner = styled.Image.attrs({
+  resizeMode: "contain",
+})`
+  width: 100%;
+`;
+
+export const Title = styled.Text`
+  font-family: Poppins_400Regular;
+  font-size: 20px;
+  line-height: 30px;
+  margin-top: 80px;
+  color: #fff;
+`;
+
+export const TitleBold = styled.Text`
+  font-family: Poppins_600SemiBold;
+`;
+
+export const ButtonsContainer = styled.View`
+  flex-direction: row;
+  margin-top: 40px;
+  justify-content: space-between;
+`;
+
+export const Button = styled.TouchableOpacity<IButtonProps>`
+  background-color: ${(props) => (props.primary ? "#9871f5" : "#04d361")};
+  height: 150px;
+  width: 48%;
+  border-radius: 8px;
+  padding: 24px;
+  justify-content: space-between;
+`;
+
+export const ButtonImg = styled.Image``;
+
+export const ButtonText = styled.Text`
+  font-family: Archivo_700Bold;
+  color: #fff;
+  font-size: 20px;
+`;
+
+export const Image = styled.Image``;
+
+export const TotalConnections = styled.Text`
+  font-family: Poppins_400Regular;
+  color: #d4c2ff;
+  font-size: 12px;
+  line-height: 20px;
+  max-width: 140px;
+  margin-top: 40px;
+`;
