c9fc0fe4031958ed17855dc625b81590c6a9cfab
carlos3g <carlosmesquita156@gmail.com>
2020-08-09 23:47:02 -0300

    types

--- mobile/src/@types/types.ts
+++ mobile/src/@types/types.ts
@@ -1,3 +1,31 @@
+import { ReactNode } from "react";
+
+// styled
 export interface IButtonProps {
   primary?: boolean;
+}
+
+export interface IFavoritedButtonProps {
+  favorited?: boolean;
+}
+
+// components
+export interface IPageHeaderProps {
+  title: string;
+  headerRight?: ReactNode;
+}
+
+export interface ITeacherItemProps {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+export interface IProffy {
+  proffy: ITeacherItemProps;
+  favorited: boolean;
 }
